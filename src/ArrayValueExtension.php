94cd8a3faf66b8061a107cb000878f5c746006bd
Joossensei <jj.both03@gmail.com>
2022-01-18 09:52:31 +0100

    Wrote function that runs a foreach loop
    In twig it would be messy to write a foreach loop for every answer. So I made it a custom function.

---
+++ src/ArrayValueExtension.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+class ArrayValueExtension extends AbstractExtension
+{
+    public function getFunctions()
+    {
+        return [
+            new TwigFunction('has_value', [$this, 'hasValue'])
+        ];
+    }
+
+    public function hasValue(array $array, string $key): bool
+    {
+        foreach($array as $value) {
+            if($value == $key) {
+                dump($value);
+                return true;
+            } else {
+                continue;
+            }
+        }
+        return false;
+    }
+
+}
